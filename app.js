461b8d88c1b7480ffad67df4593a87f4ef042d8d
Leonardo Henrique Batista <leonardo@lhbdigital.com.br>
2021-06-09 23:09:40 -0300

    added contacts endpoints

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Hello World
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -11,6 +12,13 @@ app.use(express.static('public'))
 
 // Served on /static
 app.use('/static', express.static('public'))
+
+// Contact endpoints
+app.get('/contacts', (req, res) => { res.send('Enviando todos os contatos') })
+app.get('/contacts/:id', (req, res) => { res.send('Enviando um contato específico') })
+app.post('/contacts', (req, res) => { res.send('Criando um contato') })
+app.put('/contacts/:id', (req, res) => { res.send('Alterando um contato') })
+app.delete('/contacts/:id', (req, res) => { res.send('Deletando um contato') })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
