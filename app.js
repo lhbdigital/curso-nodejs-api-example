bd535f71019350b10d4f571d3ec8237eaf7be3ca
Leonardo Henrique Batista <leonardo@lhbdigital.com.br>
2021-06-09 22:34:41 -0300

    static files example

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// Served on root
+app.use(express.static('public'))
+
+// Served on /static
+app.use('/static', express.static('public'))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
