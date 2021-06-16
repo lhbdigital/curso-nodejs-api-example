2e2c1c3ff7de1e1d4a6af86490cf7b7328fc6ba7
Leonardo Henrique Batista <leonardo@lhbdigital.com.br>
2021-06-15 23:43:10 -0300

    Updated endpoints with DB queries

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,98 @@
 const express = require('express')
 const router = express()
 
-router.get('/', (req, res) => { res.send('Enviando todos os contatos') })
-router.get('/:id', (req, res) => { res.send('Enviando um contato específico') })
-router.post('/', (req, res) => { res.send('Criando um contato') })
-router.put('/:id', (req, res) => { res.send('Alterando um contato') })
-router.delete('/:id', (req, res) => { res.send('Deletando um contato') })
+const executeQuery = require('../lib/database')
+
+router.get('/', async (req, res) => {
+  try {
+    console.log('Getting contacts');
+    const contacts = await executeQuery(`SELECT * FROM contacts;`)
+    res.send(contacts) 
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error getting contacts')
+  }
+})
+
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const intId = parseInt(id)
+  
+  if(!id || isNaN(intId)) {
+    res.status(400).send('Missing os incorrect id')
+  } else { 
+    try {
+      console.log(`Getting contact with id=${intId}`);
+      const contacts = await executeQuery(`SELECT * FROM contacts WHERE id=${intId};`)
+      res.send(contacts) 
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(`Error getting contact with id=${intId}`)
+    }
+  }
+})
+
+
+router.post('/', async (req, res) => {
+  const { name, phone, description } = req.body
+
+  try {
+    console.log(`Creating contact`);
+    
+    await executeQuery(
+      `INSERT INTO contacts (name,phone,description)` +
+      `VALUES ('${name || ''}','${phone || ''}','${description || ''}');`
+    )
+    res.send() 
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(`Error creating contact`)
+  }
+})
+
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const intId = parseInt(id)
+
+  const { name, phone, description } = req.body
+  
+  if(!id || isNaN(intId)) {
+    res.status(400).send('Missing os incorrect id')
+  } else { 
+    try {
+      console.log(`Updating contact with id=${intId}`);
+
+      await executeQuery(
+        `UPDATE contacts SET name='${name || ''}', phone='${phone || ''}', ` +
+        `description='${description || ''}' WHERE id=${intId};`
+      )
+      res.send() 
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(`Error updating contact with id=${intId}`)
+    }
+  }
+})
+
+
+router.delete('/:id', async (req, res) => { 
+  const { id } = req.params
+  const intId = parseInt(id)
+  
+  if(!id || isNaN(intId)) {
+    res.status(400).send('Missing os incorrect id')
+  } else { 
+    try {
+      console.log(`Deleting contact with id=${intId}`);
+      await executeQuery(`DELETE FROM contacts WHERE id=${intId};`)
+      res.send(contacts) 
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(`Error deleting contact with id=${intId}`)
+    }
+  }
+})
 
 module.exports = router;
